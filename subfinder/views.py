da1ebe7f3bce4563fc7561d2a4045f749893ab1a
Raghav Sharma <raghav_ftw@hotmail.com>
2016-01-24 03:10:26 +0530

    Added endpoint get_hash. Atm just sends back what received in JSON.

--- subfinder/views.py
+++ subfinder/views.py
@@ -1,7 +1,13 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from subfinder import app
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/api/hash")
+def get_hash():
+    data = {'file': request.args['fileName'], 'hash': request.args['hash']}
+    return jsonify(data), 200
