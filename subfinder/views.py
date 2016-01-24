49b5032733f6b9cab15aad5d7b8e0a9ba572070a
Raghav Sharma <raghav_ftw@hotmail.com>
2016-01-25 02:30:12 +0530

    Using OMDb API to get plot and poster image from IMDb id.

--- subfinder/views.py
+++ subfinder/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, jsonify
 from subfinder import app
 import opensubtitles
-
+import requests
 
 @app.route("/")
 def index():
@@ -15,5 +15,17 @@ def get_hash():
     file_size = request.args['fileSize']
     search_data = [{'moviehash': hash, 'moviebytesize': file_size, 'sublanguageid': 'eng'}]
     sub_data = opensubtitles.searchSub(search_data)
+    for data in sub_data:
+        imdb_data = _get_imdb_data(data['IDMovieImdb'])
+        data['plot'] = imdb_data['Plot']
+        data['poster'] = imdb_data['Poster']
     result = {'file': file_name, 'data': sub_data}
     return jsonify(result), 200
+
+
+def _get_imdb_data(imdb_id):
+    imdb_id = "tt" + "%07d" % int(imdb_id)
+    payload = {"i": imdb_id, "plot": "full"}
+    r = requests.get("http://www.omdbapi.com", params=payload)
+    imdb_data = r.json()
+    return imdb_data
