47933101c4b8a925b45cc76b69995978d5bf8b03
Raghav Sharma <raghav_ftw@hotmail.com>
2016-01-28 18:51:19 +0530

    Refactored opensubtitles.py

--- subfinder/opensubtitles.py
+++ subfinder/opensubtitles.py
@@ -20,8 +20,10 @@
 # GNU General Public License for more details                                  #
 ################################################################################
 import collections
-
+import operator
+import itertools
 import xmlrpclib
+import logging
 
 server_url = "http://api.opensubtitles.org/xml-rpc"
 user_agent = "OSTestUserAgent"  # Test user agent, you should request a new one.
@@ -32,26 +34,28 @@ server = xmlrpclib.Server(server_url)
 # This is our custom rating algorithm, to find the best suitable sub
 # in a list of dictionaries. It inserts our calculated rating value in
 # the dictionary and returns the list.
-def ratingAlgorithm(data):
+def rating_algorithm(data):
     for i in data:
-        i['ratingAlgo'] = 0
+        i['rating_algo'] = 0
         if int(i['SubBad']) > 0:
-            i['ratingAlgo'] -= 5
+            i['rating_algo'] -= 5
         rating = float(i['SubRating'])
         if 4.0 > rating > 0.0:
-            i['ratingAlgo'] -= 5
+            i['rating_algo'] -= 5
         else:
-            i['ratingAlgo'] += round(rating)
+            i['rating_algo'] += round(rating)
         if i['UserRank'] == "administrator" or i['UserRank'] == "trusted":
-            i['ratingAlgo'] += 1
+            i['rating_algo'] += 1
     return data
 
 
 # OpenSubtitles DB is fucked up, it returns multiple results and
 # sometimes of different movies/series. So we have to do a lot of
 # shit to find the best sub.
-def searchSub(data):
+def search_sub(data):
     try:
+        login_data = _login("eng")
+        token = login_data['token']
         result = server.SearchSubtitles(token, data)
 
         if result['status'] != "200 OK":
@@ -73,29 +77,26 @@ def searchSub(data):
         # most common to check if the count of 1 & 2 are not equal,
         # in that case we use the original result.
         c = collections.Counter(i['IDMovieImdb'] for i in data)
-        mostCommon = c.most_common(2)
-        movieCount = len(mostCommon)
+        most_common = c.most_common(2)
+        movie_count = len(most_common)
 
         # We choose the most common movie if the count of
         # 1st & 2nd are different. [0][0] is IMDb id and
         # [0][1] is count of those IMDb ids in result
-        isMostCommon = (movieCount == 1 or
-                        (movieCount > 1 and
-                         mostCommon[0][1] != mostCommon[1][1]))
-        if isMostCommon:
-            data = [i for i in data if i['IDMovieImdb'] == mostCommon[0][0]]
+        is_most_common = (movie_count == 1 or
+                          (movie_count > 1 and
+                           most_common[0][1] != most_common[1][1]))
+        if is_most_common:
+            data = [i for i in data if i['IDMovieImdb'] == most_common[0][0]]
 
-        data = ratingAlgorithm(data)
+        data = rating_algorithm(data)
 
         # We sort the data on the basis of our rating algorithm
         # and sub add date(Assuming the latest sub would be better)
-        sortedData = sorted(data,
-                            key=lambda k: (float(k['ratingAlgo']),
-                                           k['SubAddDate']),
-                            reverse=True)
-        return sortedData
+        sorted_data = sorted(data, key=lambda k: (float(k['rating_algo']), k['SubAddDate']), reverse=True)
+        return sorted_data
     except Exception as e:
-        print ('An error occured while searching sub: %s') % e
+        logging.error("An error occurred while searching sub: %s" % e)
 
 
 # This will end the session id.
@@ -104,17 +105,12 @@ def _logout(token):
     try:
         server.LogOut(token)
     except Exception as e:
-        print ('An error occured while logging out: %s') % e
+        logging.error("An error occurred while logging out: %s" % e)
 
 
 def _login(lang, username="", password=""):
     try:
-        result = server.LogIn(username, password,
-                              lang, user_agent)
+        result = server.LogIn(username, password, lang, user_agent)
         return result
     except Exception as e:
-        print ('An error occured while logging in: %s') % e
-
-
-loginData = _login("eng")
-token = loginData['token']
+        logging.error("An error occurred while logging in: %s" % e)
